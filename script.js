067e3a0476685227dd06ef39316c03ca46246052
Order-Corner <100425817+Order-Corner@users.noreply.github.com>
2022-08-07 20:13:55 -0700

    Deleted itemsToAttribute because we bought the license Added CPS to make the site more secure Outsourced therefore all inline Styles

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("menuButton").addEventListener("click", function () {
+  document.getElementById("body").classList.toggle("menuactive");
+});
+document.getElementById("mehrClicked").addEventListener("click", function () {
+  document.getElementById("body").classList.toggle("menuactive");
+});
+document
+  .getElementById("rechtlichesLink")
+  .addEventListener("click", function () {
+    document.getElementById("body").classList.toggle("menuactive");
+  });
