70a4179d89185c7eaffb203294926cf92a41495a
Order-Corner <100425817+Order-Corner@users.noreply.github.com>
2022-08-14 22:11:49 -0700

    Changed Video Scripts to work with CSP

--- loadVideo.js
+++
@@ -1,6 +0,0 @@
-function displayIframe(id) {
-  document.getElementById("iframeDisplay").innerHTML =
-    '<iframe src="https://www.youtube.com/embed/' +
-    id +
-    '" title="Erklärvideo" loading="lazy" frameborder="0"></iframe>';
-}
