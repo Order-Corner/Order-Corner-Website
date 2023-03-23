4ebd0b05848640e6380654244e4dbad668c597e3
MaxEckstein <79403576+Boothosh@users.noreply.github.com>
2023-03-23 22:52:01 +0100

    CP verändert und discord link entfernt

--- socialAnimation.js
+++ socialAnimation.js
@@ -1,7 +1,7 @@
 var head = document.getElementsByTagName("head")[0];
 var script = document.createElement("script");
 script.type = "text/javascript";
-script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
+script.src = "https://code.jquery.com/jquery-3.6.1.slim.min.js";
 script.onreadystatechange = handler;
 script.onload = handler;
 
